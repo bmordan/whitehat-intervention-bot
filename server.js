fe766d59cb44ab159de295318e3ef4938c954057
bmordan <bernardmordan@gmail.com>
2019-10-14 20:44:09 +0100

    rise bot

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.set('port', process.env.PORT || 9292)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.post("/", (req, res) => {
+    const {challenge} = req.body
+    res.send({challenge})
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Intervention Bot alive on port ${app.get('port')}`)
+})
