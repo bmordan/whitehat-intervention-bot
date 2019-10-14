bb1f5d1fa19963eceefdb0602edccd1dc6b069b2
bmordan <bernardmordan@gmail.com>
2019-10-14 23:08:57 +0100

    stops recursion

--- server.js
+++ server.js
@@ -6,22 +6,30 @@ app.set('port', process.env.PORT || 9292)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.post('/challenge', (req, res) => {
-    console.log("FROM SLACK", req.body)
-    res.send()
-    request.post({
-        uri: 'https://slack.com/api/chat.postMessage',
-        headers: {
-            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
-        },
-        json: {
-            text: "Hello you. I'm @WhiteHatBot",
-            channel: '#test_intervention_bot'
+function bot_reply({type, text}) {
+    const bot_replies = {
+        message: () => {},
+        app_mention: () => {
+            request.post({
+                uri: 'https://slack.com/api/chat.postMessage',
+                headers: {
+                    Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+                },
+                json: {
+                    text: "Hello you. I'm @WhiteHatBot",
+                    channel: '#test_intervention_bot'
+                }
+            }, (err, response, body) => {
+                if (err) return console.error(err)
+                console.log("AFTER POST TO SLACK", body)
+            })
         }
-    }, (err, response, body) => {
-        if (err) return console.error(err)
-        console.log("AFTER POST TO SLACK", body)
-    })
+    }
+}
+
+app.post('/challenge', (req, res) => {
+    res.send()
+    bot_reply(req.body.event)
 })
 
 app.listen(app.get('port'), () => {
