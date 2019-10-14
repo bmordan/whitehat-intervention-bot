632b3de475ba87d44ee40cd57a9276e5bf151da6
bmordan <bernardmordan@gmail.com>
2019-10-15 00:37:26 +0100

    more simple chasing the hello replies

--- server.js
+++ server.js
@@ -11,14 +11,11 @@ app.use(express.json())
 
 function err (err) { console.error(err) }
 
-let last_message;
-
 function bot_reply({username, text, subtype, type}) {
     console.log({username, text, last_message, subtype, type})
     if (subtype === 'bot_message') return
-    if (last_message === text && type !== 'app_mention') return
     
-    const input = type === 'app_mention' ? "__hi" : text
+    const input = type === 'app_mention' ? '__hi' : text
     
     bot.reply(username, input).then(reply => {
         request.post({
@@ -26,8 +23,6 @@ function bot_reply({username, text, subtype, type}) {
             headers: { Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}` },
             json: { text: reply, channel }
         }, err)
-
-        last_message = text
     })
 }
 
