7850d184bf1ca968754f5434fd4a1a90e6c9d827
bmordan <bernardmordan@gmail.com>
2019-10-14 21:38:32 +0100

    adds a bot token to env

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ app.set('port', process.env.PORT || 9292)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.post("/challenge", (req, res) => {
+app.post('/challenge', (req, res) => {
     const {challenge} = req.body
     res.send({challenge})
+})
+
+app.post('/', (req, res) => {
+    res.setHeader('Authorization', `Bearer ${process.env.SLACK_BOT_TOKEN}`)
+    res.send({
+        text: `Hello you`,
+        channel: 'ANZSTAXUJ'
+    })
 })
 
 app.listen(app.get('port'), () => {
